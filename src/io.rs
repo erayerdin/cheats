05a78413606637c319a558ee5bc8437d098b08c7
Eray Erdin <eraygezer.94@gmail.com>
2020-06-14 21:09:20 +0300

    added Read for Stream

--- src/io.rs
+++ src/io.rs
@@ -24,3 +24,57 @@ impl Stream {
         Stream { buffer: vec![] }
     }
 }
+
+impl io::Read for Stream {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        let length = buf.len();
+
+        let mut byte_count = 0usize;
+        for i in 0..length {
+            match self.buffer.get(0) {
+                Some(byte) => {
+                    buf[i] = byte.clone();
+                    byte_count += 1;
+                    self.buffer.remove(0);
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+
+        Ok(byte_count)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+    use std::io::Read;
+
+    #[fixture]
+    fn stream() -> Stream {
+        let mut stream = Stream::new();
+        stream.buffer.extend(b"lorem");
+        stream
+    }
+
+    #[rstest]
+    fn read(mut stream: Stream) {
+        let mut msg = String::new();
+        let mut buffer: [u8; 8] = [0; 8];
+
+        let byte_count = stream.read(&mut buffer).expect("Could not read Stream.");
+        msg.extend(
+            buffer
+                .iter()
+                .filter(|n| **n != 0u8)
+                .map(|n| n.clone() as char),
+        );
+
+        assert_eq!(byte_count, 5);
+        assert_eq!(msg, "lorem");
+        assert!(stream.buffer.is_empty());
+    }
+}
